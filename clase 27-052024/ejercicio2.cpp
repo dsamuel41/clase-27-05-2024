5b8d63adf1a515a27fba2bfb8da3ebc9a5823b80
dsamuel41 <164080389+dsamuel41@users.noreply.github.com>
2024-05-27 12:32:24 -0600

    Add files via upload

---
+++ clase 27-052024/ejercicio2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+    int n=0;
+    cout << "Ingrese tamaño del arreglo" "\n",
+    cin >>n;
+
+    int num[n];
+    int par[n];
+    int j=0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Ingrese un numero entero" "\n";
+        cin>>num[i];
+        if (num[i] % 2 == 0)
+        {
+            par[j]=num[i];
+            j++;
+        }
+        
+    }
+    cout <<"El arreglo original es:" "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << num [i] << "\n";
+    }
+    cout << "El arreglo de pares es:" "\n";
+    for (int k = 0; k < j; k++)
+    {
+        cout << par[k] << "\n";
+
+    }
+    
+    return 0;
+    
+}
